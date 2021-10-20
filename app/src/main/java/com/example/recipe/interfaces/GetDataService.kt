5f872f28eb1c125dab4c131630070660dd4f953b
Adil <rapper.k1ng@gmail.comp>
2021-10-20 20:27:20 +0600

    commit everything

---
+++ app/src/main/java/com/example/recipe/interfaces/GetDataService.kt
@@ -0,0 +1,22 @@
+package com.example.recipe.interfaces
+
+
+import com.example.recipe.entities.Category
+import com.example.recipe.entities.Meal
+import com.example.recipe.entities.MealResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GetDataService {
+    @GET("categories.php")
+    fun getCategoryList(): Call<Category>
+
+    @GET("filter.php")
+    fun getMealList(@Query("c") category: String): Call<Meal>
+
+    @GET("lookup.php")
+    fun getSpecificItem(@Query("i") id: String): Call<MealResponse>
+
+
+}
